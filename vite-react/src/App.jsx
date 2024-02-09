16f7fcef83f54f09378fd635b99b6588ce8dbd7a
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-02-09 15:06:54 +0530

    Create custom react library to know deep on React

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,5 +1,14 @@
+import Dev from "./Dev";
+
 function App() {
-  return <h1>First step on React with Vite & | MB </h1>;
+  const devName = "Mithu";
+  //when a const is declare in return it becomes a evaluated expression no if or else is used in evaluated expression
+  return (
+    <>
+      <h1>First step on React with Vite & | {devName} </h1>
+      <Dev />
+    </>
+  );
 }
 
 export default App;
--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
-import Dev from "./Dev.jsx";
+
+// function MyApp() {
+//   return <h2>Its just a function</h2>;
+// }
+//MyApp render like a function or like a jsx syntex like <MyApp />
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
+// );
+//anotherElement is render like a object
+
+// const reactElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   "Click here for google"
+// );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <App />
-    <Dev />
   </>
 );
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   reactElement
+// );
