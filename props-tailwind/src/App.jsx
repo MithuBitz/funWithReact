d4ba77be067be385d59c7869091c51a66608f6ab
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-02-12 20:17:05 +0530

    all about props

--- props-tailwind/src/App.jsx
+++ props-tailwind/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Card from "./components/Card.component";
 
 function App() {
-  let myObj = { username: "Mibits", age: 32 };
-  let newArr = [1, 2, 3];
+  //btn text for username Mithu will be set as default declare in Card component level
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind Test
       </h1>
-      <Card username="mibits-tech" someObj={myObj} alsoObj={newArr} />
+      <Card username="mibits-tech" btnText="Click me" />
+      <Card username="Mithu" />
     </>
   );
 }
--- props-tailwind/src/components/Card.component.jsx
+++ props-tailwind/src/components/Card.component.jsx
@@ -1,5 +1,5 @@
-function Card(props) {
-  console.log("Props: ", props);
+function Card({ username, btnText="visit me" }) {
+  //   console.log("Props: ", props.username);
   return (
     <>
       <div className="relative h-[400px] w-[300px] rounded-md">
@@ -10,13 +10,13 @@ function Card(props) {
         />
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
         <div className="absolute bottom-4 left-4 text-left">
-          <h1 className="text-lg font-semibold text-white">Delba</h1>
+          <h1 className="text-lg font-semibold text-white">{username}</h1>
           <p className="mt-2 text-sm text-gray-300">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
             debitis?
           </p>
           <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-            View Profile →
+            {btnText}
           </button>
         </div>
       </div>
