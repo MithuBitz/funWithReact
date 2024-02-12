050f9250a2bd6d1d15dec390df797e3e383aee76
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-02-12 19:25:03 +0530

    props is used as a object

--- props-tailwind/src/App.jsx
+++ props-tailwind/src/App.jsx
@@ -1,33 +1,15 @@
 import "./App.css";
+import Card from "./components/Card.component";
 
 function App() {
+  let myObj = { username: "Mibits", age: 32 };
+  let newArr = [1, 2, 3];
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind Test</h1>
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src="https://images.pexels.com/photos/2159543/pexels-photo-2159543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div className="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
-        </div>
-      </figure>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      <Card username="mibits-tech" someObj={myObj} alsoObj={newArr} />
     </>
   );
 }
