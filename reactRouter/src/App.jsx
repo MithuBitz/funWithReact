13954c266d5acb4879441e0b282d56eaee6c1f45
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-02-17 00:32:59 +0530

    react router dom implementation

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-4 bg-green-800">React Router</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { About, Home, Contact, User } from "../src/components";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userId" element= { <User /> } />
+      <Route path="github" loader= {githubInfoLoader} element={ <Github />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
